a31d8e3d8de4bb4a9f98fd2e8df970f5f0d0f190
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-04-14 18:52:12 +0530

    dynamic routes & req handlers

--- src/app.js
+++ src/app.js
@@ -3,23 +3,11 @@ const express = require("express");
 const app = express();
 
 // This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
     res.send({ firstName: "Harshal", lastName: "Shirke" });
 })
 
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database!");
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully!")
-})
-
-// this will match all the HTTP method API call to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
-})
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
