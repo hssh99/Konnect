44968110ec411d97a678a1e40c892e5ed0aa6b26
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-05-18 19:03:31 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,55 +1,31 @@
 const express = require("express");
-
+const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user");
 
-// const { adminAuth, userAuth } = require("./middlewares/auth")
-
-// app.use("/route", rH, rH2, rH3, rH4, rH5);
-
-// app.use(
-//     "/user",
-//     (req, res, next) => {
-//         console.log("Handling the route user!!");
-//         // next();
-//         res.send("Response!!");
-//     },
-//     (req, res) => {
-//         console.log("Handling the route user 2!!");
-//         res.send("2nd Response");
-//     }
-// )
-
-// Handle Auth Middleware for all GET, POST, ... requests
-// app.use("/admin", adminAuth)
-
-// app.get("/admin/getAllData", (req, res) => {
-//     res.send("All Data Sent");
-// })
-
-// app.get("/admin/deleteUser", (req, res) => {
-//     res.send("Deleted a user");
-// })
-
-app.get("/getUserData", (req, res) => {
+app.post("/signup", async (req, res) => {
+    // Creating a new instance of the User model
+    const user = new User({
+        firstName: "Sachin",
+        lastName: "Tendulkar",
+        emailId: "sachin@tendulkar.com",
+        password: "sachin@123"
+    });
     try {
-        // Logic of DB call and get user data
-
-        throw new Error("dvjnnf");
-
-        res.send("User Data Sent");
+        await user.save();
+        res.send("User added successfully!")
     } catch (error) {
-        res.status(500).send("Some Error contact support team");
-    }
-
-})
-
-app.use("/", (err, req, res, next) => {
-    if (err) {
-        // Log your error
-        res.status(500).send("something went wrong");
+        res.status(400).send("Error saving the user:" + err.message);
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000...")
-});
+connectDB()
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(3000, () => {
+            console.log("Server is successfully listening on port 3000...")
+        });
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected!!");
+    })
