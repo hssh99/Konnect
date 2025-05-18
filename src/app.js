8d93507bd962c9639c48c72a1e63f02ce6c50ba3
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-05-18 13:14:47 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-    "/user",
-    (req, res, next) => {
-        console.log("Handling the route user!!");
-        res.send("Response!!");
-        next();
-    },
-    (req, res) => {
-        console.log("Handling the route user 2!!");
-        res.send("2nd Response");
-    }
-)
+const { adminAuth, userAuth } = require("./middlewares/auth")
+
+// app.use("/route", rH, rH2, rH3, rH4, rH5);
+
+// app.use(
+//     "/user",
+//     (req, res, next) => {
+//         console.log("Handling the route user!!");
+//         // next();
+//         res.send("Response!!");
+//     },
+//     (req, res) => {
+//         console.log("Handling the route user 2!!");
+//         res.send("2nd Response");
+//     }
+// )
+
+// Handle Auth Middleware for all GET, POST, ... requests
+app.use("/admin", adminAuth)
+
+app.get("/user", userAuth, (req, res) => {
+    res.send("User Data Sent");
+})
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All Data Sent");
+})
+
+app.get("/admin/deleteUser", (req, res) => {
+    res.send("Deleted a user");
+})
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
