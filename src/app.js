f598622ea7b7368d03bc6b9db641744d86386cb7
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-05-24 18:29:44 +0530

    add: user schema methods

--- src/app.js
+++ src/app.js
@@ -45,15 +45,11 @@ app.post("/login", async (req, res) => {
             throw new Error("Invalid credentials")
         }
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid = await user.validatePassword(password);
 
         if (isPasswordValid) {
-
-            // Create a JWT Token
-            const token = await jwt.sign({ _id: user._id }, "DEV@Konnect$790");
-
-            // Add the token to cookie and send the response back to the user
-            res.cookie("token", token);
+            const token = await user.getJWT();
+            res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
             res.send("Login Successful!!");
         } else {
             throw new Error("Invalid credentials");
@@ -72,89 +68,13 @@ app.get("/profile", userAuth, async (req, res) => {
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
-
-
 })
 
-// Get user by email
-app.get("/user", async (req, res) => {
-    const userEmail = req.body.emailId;
-
-    try {
-        // find only one emailId
-        const user = await User.findOne({ emailId: userEmail });
-        if (!user) {
-            res.status(404).send("User not found");
-        }
-        res.send(user);
-
-        // const users = await User.find({ emailId: userEmail });
-        // if (users.length === 0) {
-        //     res.status(404).send("User not found");
-        // }
-        // res.send(users);
-    } catch (error) {
-        res.status(400).send("Something went wrong");
-    }
-
-
-})
-
-// Feed API - GET /feed = get all the users from the db
-app.get("/feed", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (error) {
-        res.status(400).send("Something went wrong");
-    }
-})
-
-// Delete a user from the database
-app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
-    try {
-        const user = await User.findByIdAndDelete(userId);
-
-        res.send("User deleted successfully");
-    } catch (error) {
-        res.status(400).send("Something went wrong");
-    }
-})
-
-// Update data of the user
-app.patch("/user/:userId", async (req, res) => {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-
-
-    try {
-        const ALLOWED_UPDATES = [
-            "photoUrl", "about", "gender", "age", "skills"
-        ]
-
-        const isUpdateAllowed = Object.keys(data).every((k) =>
-            ALLOWED_UPDATES.includes(k));
-
-        if (!isUpdateAllowed) {
-            throw new Error("Update not allowed");
-        }
-        if (data?.skills.length > 10) {
-            throw new Error("Skills cannot be more than 10");
-        }
-
-        const user = await User.findByIdAndUpdate({ _id: userId }, data,
-            {
-                returnDocument: "after",
-                runValidators: true
-            }
-        );
-        console.log(user);
-        res.send("User updated successfully");
-    } catch (error) {
-        res.status(400).send("UPDATE FAILED: " + error.message);
-    }
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+    const user = req.user;
+    // Sending a connection request
+    console.log("Sending a connection request");
+    res.send(user.firstName + " sent the Connection Request!");
 })
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -62,6 +64,20 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.getJWT = async function () {
+    const user = this;
+    const token = await jwt.sign({ _id: user._id }, "DEV@Konnect$790", {
+        expiresIn: "1d"
+    });
+    return token;
+}
 
-module.exports = User;
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
+    return isPasswordValid;
+}
+
+module.exports = mongoose.model("User", userSchema);;
