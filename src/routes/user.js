ed37bf66f86721698b2c542764cbcf5ad0ef1eab
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-05-28 00:22:17 +0530

    add: user connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
@@ -19,6 +21,26 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         })
     } catch (err) {
         req.statusCode(400).send("ERROR: " + err.message);
+    }
+})
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => row.fromUserId);
+
+        res.json({ data: data });
+
+    } catch (err) {
+        res.status(400).send({ message: err.message });
     }
 })
 
