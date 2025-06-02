4aad8e2c84f8032618fe92bbb35b4d19e53764e2
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-06-02 21:08:36 +0530

    fix: mongodb id equality check in connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -33,11 +33,17 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
                 { toUserId: loggedInUser._id, status: "accepted" },
                 { fromUserId: loggedInUser._id, status: "accepted" },
             ]
-        }).populate("fromUserId", USER_SAFE_DATA);
+        }).populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA);
 
-        const data = connectionRequests.map((row) => row.fromUserId);
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
 
-        res.json({ data: data });
+        res.json({ data });
 
     } catch (err) {
         res.status(400).send({ message: err.message });
